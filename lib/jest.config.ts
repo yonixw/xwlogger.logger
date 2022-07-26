421f8dc10285010025744826b75399dcba00a8c4
Yoni Water Man <yonixw@gmail.com>
2022-07-26 14:12:24 +0000

    coverage stuff

--- lib/jest.config.ts
+++ lib/jest.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "@jest/types";
 // Sync object
 const config: Config.InitialOptions = {
+  testPathIgnorePatterns: ["dist/", "lib/dist/"],
+  coveragePathIgnorePatterns: ["dist/", "lib/dist/"],
+  collectCoverage: true,
   verbose: true,
   transform: {
     "^.+\\.tsx?$": "ts-jest",
