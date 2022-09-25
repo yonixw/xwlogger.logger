d58b482252995ab30280e90544cbf3e6dd58f28c
Yoni Water Man <yonixw@gmail.com>
2022-09-25 00:09:53 +0000

    get stack parts.. for auto log hier?

--- src/modifiers/runtime/CallStackFuncName.ts
+++ src/modifiers/runtime/CallStackFuncName.ts
@@ -2,6 +2,9 @@ import { LogMessage } from "../../consts/logs";
 import { AnyDict } from "../../utils/ts";
 import { ModRuntimeBase } from "./mod_runtime_base";
 
+//.exec("  at B (/go/home:123:22:22)") ==>  [B, /go/home, 123:22:22]
+const getLineParts = /\s+at\s*([^\s]+)\s*\(([^:]+):([^\)]+)\)/g;
+
 const startPath = process.cwd();
 function _get_stack_item(stack: string, i: number) {
   // good for ts-node
@@ -9,6 +12,18 @@ function _get_stack_item(stack: string, i: number) {
   //    need tsconfig "inlineSourceMap" + node flag --enable-source-maps
   const lines = stack.split("\n").filter((e) => /^\s+at/.test(e));
   if (lines.length < i) return "";
+
+  getLineParts.lastIndex = 0;
+  const pathParts = getLineParts.exec(lines[i]);
+  if (pathParts && pathParts.length >= 4) {
+    return [
+      pathParts[1],
+      pathParts[2].replace(startPath, ""),
+      pathParts[3],
+    ].join(", ");
+  }
+
+  // Fallback:
   const path = lines[i].replace(/\s+at\s*/, "").replace(startPath, "");
   return path;
 }
